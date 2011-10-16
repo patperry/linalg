f60939b8278dc20ee2b84c641898b0df51fa69de
Patrick Perry <patperry@gmail.com>
2011-10-16 09:10:31 -0700

    Fix bug in matrix_dtrans

--- src/matrixutil.c
+++ src/matrixutil.c
@@ -8,6 +8,6 @@ matrix_dtrans(size_t m, size_t n, const double *a, size_t lda,
 	size_t j;
 
 	for (j = 0; j < n; j++) {
-		blas_dcopy(m, a + j * lda, 1, b + j * ldb, 1);
+		blas_dcopy(m, a + j * lda, 1, b + j, ldb);
 	}
 }
