454f4dfa872acba469d15145806cc18277393f91
Patrick Perry <patperry@gmail.com>
2011-10-24 15:59:09 -0400

    Add vpattern structure to simplify sblas interface

--- src/sblas.c
+++ src/sblas.c
@@ -1,11 +1,120 @@
 #include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
+
 #include "sblas.h"
 
-/* y[indx[i]] += alpha * x[i] for i = 0, ..., nz-1 */
-void sblas_daxpyi(size_t nz, double alpha, const double *x,
-		  const size_t *indx, double *y)
+extern void xalloc_die(void);
+
+static void *xrealloc(void *ptr, size_t size)
 {
+        void *res = realloc(ptr, size);
+        if (!res) {
+                xalloc_die();
+        }
+        return res;
+}
+
+
+/* 0, 5, 11, 20, 34, 55, 86, 133, 203, 308, ... */
+#define ARRAY_DELTA(n) \
+        ((n) ? ((n) >> 1) + 4 : 5)
+
+#define ARRAY_GROW(n,nmax) \
+        (((n) <= (nmax) - ARRAY_DELTA(n)) \
+         ? (n) + ARRAY_DELTA(n) \
+         : (nmax))
+
+void vpattern_init(struct vpattern *pat)
+{
+	pat->indx = NULL;
+	pat->nz = 0;
+	pat->nzmax = 0;
+}
+
+void vpattern_deinit(struct vpattern *pat)
+{
+	free(pat->indx);
+}
+
+void vpattern_clear(struct vpattern *pat)
+{
+	pat->nz = 0;
+}
+
+size_t vpattern_grow(struct vpattern *pat, size_t delta)
+{
+	assert(delta <= SIZE_MAX - pat->nz);
+	size_t nz0 = pat->nz;
+	size_t nz = nz0 + delta;
+	size_t nzmax = pat->nzmax;
+
+	if (nz <= nzmax) {
+		return 0;
+	}
+
+	while (nzmax < nz) {
+		nzmax = ARRAY_GROW(nzmax, SIZE_MAX);
+	}
+
+	pat->indx = xrealloc(pat->indx, nzmax * sizeof(pat->indx[0]));
+	pat->nzmax = nzmax;
+	return nzmax;
+}
+
+ptrdiff_t vpattern_find(const struct vpattern *pat, size_t i)
+{
+	const size_t *base = pat->indx, *ptr;
+	size_t nz;
+
+	for (nz = pat->nz; nz != 0; nz >>= 1) {
+		ptr = base + (nz >> 1);
+		if (i == *ptr)
+			return ptr - pat->indx;
+		if (i > *ptr) {
+			base = ptr + 1;
+			nz--;
+		}
+	}
+
+	ptrdiff_t ix = base - pat->indx;
+	return ~ix;
+}
+
+size_t vpattern_search(struct vpattern *pat, size_t i, int *insp)
+{
+	const size_t *base = pat->indx, *ptr;
+	size_t nz;
+
+	for (nz = pat->nz; nz != 0; nz >>= 1) {
+		ptr = base + (nz >> 1);
+		if (i == *ptr) {
+			*insp = 0;
+			return ptr - pat->indx;
+		}
+		if (i > *ptr) {
+			base = ptr + 1;
+			nz--;
+		}
+	}
+
+	ptrdiff_t ix = base - pat->indx;
+	vpattern_grow(pat, 1);
+	memmove(pat->indx + ix + 1, pat->indx + ix,
+		(pat->nz - ix) * sizeof(pat->indx[0]));
+	pat->indx[ix] = i;
+	pat->nz++;
+	*insp = 1;
+	return ix;
+}
+
+/* y[indx[i]] += alpha * x[i] for i = 0, ..., nz-1 */
+void sblas_daxpyi(double alpha, const double *x, const struct vpattern *pat,
+		  double *y)
+{
+	size_t nz = pat->nz;
+	const size_t *indx = pat->indx;
 	size_t i;
 
 	for (i = 0; i < nz; i++) {
@@ -14,9 +123,11 @@ void sblas_daxpyi(size_t nz, double alpha, const double *x,
 }
 
 /* x[0] * y[indx[0]] + ... + x[nz-1] * y[indx[nz-1]] */
-double sblas_ddoti(size_t nz, const double *x, const size_t *indx,
+double sblas_ddoti(const double *x, const struct vpattern *pat,
 		   const double *y)
 {
+	size_t nz = pat->nz;
+	const size_t *indx = pat->indx;
 	double dot = 0.0;
 	size_t i;
 
@@ -28,8 +139,10 @@ double sblas_ddoti(size_t nz, const double *x, const size_t *indx,
 }
 
 /* x[i] := y[indx[i]] for i = 0, ..., nz-1 */
-void sblas_dgthr(size_t nz, const double *y, double *x, const size_t *indx)
+void sblas_dgthr(const double *y, double *x, const struct vpattern *pat)
 {
+	size_t nz = pat->nz;
+	const size_t *indx = pat->indx;
 	size_t i;
 
 	for (i = 0; i < nz; i++) {
@@ -38,8 +151,10 @@ void sblas_dgthr(size_t nz, const double *y, double *x, const size_t *indx)
 }
 
 /* x[i] := y[indx[i]]; * y[indx[i]] := 0 for i = 0, ..., nz-1 */
-void sblas_dgthrz(size_t nz, double *y, double *x, const size_t *indx)
+void sblas_dgthrz(double *y, double *x, const struct vpattern *pat)
 {
+	size_t nz = pat->nz;
+	const size_t *indx = pat->indx;
 	size_t i;
 
 	for (i = 0; i < nz; i++) {
@@ -49,8 +164,10 @@ void sblas_dgthrz(size_t nz, double *y, double *x, const size_t *indx)
 }
 
 /* y[indx[i]] = x[i] for i = 0, ..., nz-1 */
-void sblas_dsctr(size_t nz, const double *x, const size_t *indx, double *y)
+void sblas_dsctr(const double *x, const struct vpattern *pat, double *y)
 {
+	size_t nz = pat->nz;
+	const size_t *indx = pat->indx;
 	size_t i;
 
 	for (i = 0; i < nz; i++) {
@@ -58,9 +175,9 @@ void sblas_dsctr(size_t nz, const double *x, const size_t *indx, double *y)
 	}
 }
 
-void sblas_dgemvi(enum blas_trans trans, size_t m, size_t n, size_t nz,
+void sblas_dgemvi(enum blas_trans trans, size_t m, size_t n,
 		  double alpha, const double *a, size_t lda, const double *x,
-		  const size_t *indx, double beta, double *y)
+		  const struct vpattern *pat, double beta, double *y)
 {
 	size_t ny = (trans == BLAS_NOTRANS ? m : n);
 
@@ -71,6 +188,8 @@ void sblas_dgemvi(enum blas_trans trans, size_t m, size_t n, size_t nz,
 	}
 
 	if (trans == BLAS_NOTRANS) {
+		size_t nz = pat->nz;
+		const size_t *indx = pat->indx;
 		size_t i;
 
 		for (i = 0; i < nz; i++) {
@@ -80,27 +199,9 @@ void sblas_dgemvi(enum blas_trans trans, size_t m, size_t n, size_t nz,
 		size_t j;
 
 		for (j = 0; j < n; j++) {
-			y[j] += alpha * sblas_ddoti(nz, x, indx, a + j * lda);
+			y[j] += alpha * sblas_ddoti(x, pat, a + j * lda);
 		}
 	}
-}
-
-ptrdiff_t sblas_find(size_t nz, const size_t *indx, size_t i)
-{
-	const size_t *base = indx, *ptr;
-
-	for (; nz != 0; nz >>= 1) {
-		ptr = base + (nz >> 1);
-		if (i == *ptr)
-			return ptr - indx;
-		if (i > *ptr) {
-			base = ptr + 1;
-			nz--;
-		}
-	}
-
-	ptrdiff_t ix = base - indx;
-	return ~ix;
 }
 
 void sblas_dcscmv(enum blas_trans trans, size_t m, size_t n, double alpha,
@@ -121,16 +222,18 @@ void sblas_dcscmv(enum blas_trans trans, size_t m, size_t n, double alpha,
 			size_t nz = offa[j+1] - offa[j];
 			const double *val = a + offa[j];
 			const size_t *ind = inda + offa[j];
+			struct vpattern pat = vpattern_make(ind, nz);
 
-			sblas_daxpyi(nz, alpha * x[j], val, ind, y);
+			sblas_daxpyi(alpha * x[j], val, &pat, y);
 		}
 	} else {
 		for (j = 0; j < n; j++) {
 			size_t nz = offa[j+1] - offa[j];
 			const double *val = a + offa[j];
 			const size_t *ind = inda + offa[j];
+			struct vpattern pat = vpattern_make(ind, nz);
 
-			y[j] += alpha * sblas_ddoti(nz, val, ind, x);
+			y[j] += alpha * sblas_ddoti(val, &pat, x);
 		}
 	}
 }
--- src/sblas.h
+++ src/sblas.h
@@ -4,34 +4,55 @@
 #include <stddef.h>
 #include "blas.h"
 
+struct vpattern {
+	size_t nz, nzmax;
+	size_t *indx;
+};
+
+void vpattern_init(struct vpattern *pat);
+void vpattern_deinit(struct vpattern *pat);
+
+void vpattern_clear(struct vpattern *pat);
+size_t vpattern_grow(struct vpattern *pat, size_t delta);
+ptrdiff_t vpattern_find(const struct vpattern *pat, size_t i);
+size_t vpattern_search(struct vpattern *pat, size_t i, int *insp);
+
+static inline struct vpattern vpattern_make(const size_t *indx, size_t nz)
+{
+	struct vpattern pat;
+	pat.nz = nz;
+	pat.nzmax = nz;
+	pat.indx = (size_t *)indx;
+	return pat;
+}
+
 /* Level 1 */
 
 /* y[indx[i]] += alpha * x[i] for i = 0, ..., nz-1 */
-void sblas_daxpyi(size_t nz, double alpha, const double *x,
-		  const size_t *indx, double *y);
+void sblas_daxpyi(double alpha, const double *x, const struct vpattern *pat,
+		  double *y);
 
 /* x[0] * y[indx[0]] + ... + x[nz-1] * y[indx[nz-1]] */
-double sblas_ddoti(size_t nz, const double *x, const size_t *indx,
+double sblas_ddoti(const double *x, const struct vpattern *pat,
 		   const double *y);
 
 /* x[i] := y[indx[i]] for i = 0, ..., nz-1 */
-void sblas_dgthr(size_t nz, const double *y, double *x, const size_t *indx);
+void sblas_dgthr(const double *y, double *x, const struct vpattern *pat);
 
 /* x[i] := y[indx[i]]; * y[indx[i]] := 0 for i = 0, ..., nz-1 */
-void sblas_dgthrz(size_t nz, double *y, double *x, const size_t *indx);
+void sblas_dgthrz(double *y, double *x, const struct vpattern *pat);
 
 /* y[indx[i]] = x[i] for i = 0, ..., nz-1 */
-void sblas_dsctr(size_t nz, const double *x, const size_t *indx, double *y);
+void sblas_dsctr(const double *x, const struct vpattern *pat, double *y);
 
-ptrdiff_t sblas_find(size_t nz, const size_t *indx, size_t i);
 
 
 /* Level 2 */
 
 /* y = alpha * \sum_{i = 0}^{nz-1} { op(a)[,indx[i]] * x[i] }  +  beta * y */
-void sblas_dgemvi(enum blas_trans trans, size_t m, size_t n, size_t nz,
+void sblas_dgemvi(enum blas_trans trans, size_t m, size_t n,
 		  double alpha, const double *a, size_t lda, const double *x,
-		  const size_t *indx, double beta, double *y);
+		  const struct vpattern *pat, double beta, double *y);
 
 void sblas_dcscmv(enum blas_trans trans, size_t m, size_t n, double alpha,
 		  const double *a, const size_t *inda, const size_t *offa,
