fec6714f589b854eaaabaff45dc7d01e46d45ec1
Patrick Perry <patperry@gmail.com>
2012-11-02 14:44:36 -0400

    Add matrix_dscal{r,c}, matrix_drank

--- src/matrixutil.c
+++ src/matrixutil.c
@@ -1,5 +1,7 @@
+#include <assert.h>
 #include <string.h>
 #include "blas.h"
+#include "lapack.h"
 #include "matrixutil.h"
 
 void matrix_dzero(size_t m, size_t n, double *a, size_t lda)
@@ -26,6 +28,25 @@ void matrix_dscal(size_t m, size_t n, double alpha, double *a, size_t lda)
 		}
 	}
 }
+
+void matrix_dscalr(size_t m, size_t n, double *s, size_t incs, double *a, size_t lda)
+{
+	size_t i;
+
+	for (i = 0; i < m; i++) {
+		blas_dscal(n, s[i * incs], a + i, lda);
+	}
+}
+
+void matrix_dscalc(size_t m, size_t n, double *s, size_t incs, double *a, size_t lda)
+{
+	size_t j;
+
+	for (j = 0; j < n; j++) {
+		blas_dscal(m, s[j * incs], a + j * lda, 1);
+	}
+}
+
 
 void matrix_daxpy(size_t m, size_t n, double alpha, const double *x, size_t ldx,
 		  double *y, size_t ldy)
@@ -105,4 +126,53 @@ void packed_dsctr(enum blas_uplo uplo, size_t n, const double *ap,
 			src += len;
 		}
 	}
+}
+
+
+size_t matrix_drank_lwork(size_t m, size_t n, size_t *liwork)
+{
+	size_t k = (m <= n) ? m : n;
+
+	if (k == 0) {
+		*liwork = 0;
+		return 0;
+	}
+
+	enum lapack_svdjob jobz = LA_SVD_NOVEC;
+	size_t lwork0 = lapack_dgesdd_lwork(jobz, m, n, liwork);
+	return lwork0 + k;
+}
+
+size_t matrix_drank(size_t m, size_t n, double *a, size_t lda, double svtol,
+		    double *work, size_t lwork, ptrdiff_t *iwork)
+{
+	assert(svtol >= 0);
+	size_t k = (m <= n) ? m : n;
+	size_t rank = k;
+
+	if (k == 0)
+		goto out;
+
+	enum lapack_svdjob jobz = LA_SVD_NOVEC;
+
+	assert(lwork >= k);
+	double *s = work;
+	work += k;
+	lwork -= k;
+
+	double *u = NULL;
+	size_t ldu = 1;
+	double *vt = NULL;
+	size_t ldvt = 1;
+
+	ptrdiff_t info = lapack_dgesdd(jobz, m, n, a, lda, s, u, ldu, vt, ldvt,
+				       work, lwork, iwork);
+	assert(info >= 0); /* no illegal parameter values */
+	assert(info == 0); /* algorithm converged */
+
+	while (rank > 0 && s[rank - 1] <= svtol) {
+		rank--;
+	}
+out:
+	return rank;
 }
--- src/matrixutil.h
+++ src/matrixutil.h
@@ -4,8 +4,12 @@
 #include <stddef.h>
 #include "blas.h"
 
+#define MATRIX_DRANK_SVTOL0	(1e-8)
+
 void matrix_dzero(size_t m, size_t n, double *a, size_t lda);
 void matrix_dscal(size_t m, size_t n, double alpha, double *a, size_t lda);
+void matrix_dscalr(size_t m, size_t n, double *s, size_t incs, double *a, size_t lda);
+void matrix_dscalc(size_t m, size_t n, double *s, size_t incs, double *a, size_t lda);
 void matrix_daxpy(size_t m, size_t n, double alpha, const double *x, size_t ldx,
 		  double *y, size_t ldy);
 
@@ -16,6 +20,11 @@ void packed_dgthr(enum blas_uplo uplo, size_t n, const double *a, size_t lda,
 		  double *bp);
 void packed_dsctr(enum blas_uplo uplo, size_t n, const double *ap,
 		  double *b, size_t ldb);
+
+/* compute the numerical rank using the SVD */
+size_t matrix_drank(size_t m, size_t n, double *a, size_t lda, double svtol,
+		    double *work, size_t lwork, ptrdiff_t *iwork);
+size_t matrix_drank_lwork(size_t m, size_t n, size_t *liwork);
 
 
 #endif /* LINALG_MATRIXUTIL_H */
